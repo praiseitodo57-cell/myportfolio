c090538a5fbecf5ee3709a368f9f7c66b3694ce4
Praise Itodo <praiseitodo57@gmail.com>
2026-05-20 09:14:48 +0100

    fix: update contact details with accurate information

--- src/components/contact/ContactDetails.tsx
+++ src/components/contact/ContactDetails.tsx
@@ -3,17 +3,17 @@ import { FiPhone, FiMapPin, FiMail } from 'react-icons/fi';
 const contacts = [
 	{
 		id: 1,
-		name: 'Your Address, Your City, Your Country',
+		name: 'Your Address, Lagos, Nigeria',
 		icon: <FiMapPin />,
 	},
 	{
 		id: 2,
-		name: 'email@domain.com',
+		name: 'praiseitodo57@gmail.com',
 		icon: <FiMail />,
 	},
 	{
 		id: 3,
-		name: '555 8888 888',
+		name: '08114193235',
 		icon: <FiPhone />,
 	},
 ];
